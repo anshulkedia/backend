5deb04086974abefafa107efe5ce021afc5ec0cd
anshulkedia <anshulkedia04@gmail.com>
2024-04-16 21:49:59 +0530

    utils discussion

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+//express configurations for recieving information on the server
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(express.static("public"))
+//express configuration for cookies
+app.use(cookieParser())
+
+export { app }
